1eccf6621cf9b1f7d9996acbbacc1cc7d5011de9
EunWoo <g01023856970@gmail.com>
2026-05-20 01:52:01 +0900

    chore: initialize Spring Boot project structure

---
+++ TimeScheduling/BackEnd/src/main/java/com/example/TimeScheduling/TimeSchedulingApplication.java
@@ -0,0 +1,13 @@
+package com.example.TimeScheduling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeSchedulingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimeSchedulingApplication.class, args);
+	}
+
+}
---
+++ TimeScheduling/BackEnd/src/test/java/com/example/TimeScheduling/TimeSchedulingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TimeScheduling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimeSchedulingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
